7d1a6883070469f4464e29ed888f9ea1f8e03565
omer5144 <omer5144@gmail.com>
2024-02-03 16:06:03 -0800

    forbid weird acceleration

--- icsim/speed.c
+++ icsim/speed.c
@@ -1,12 +1,20 @@
 #include "speed.h"
-
+#include <stdio.h>
 void update_speed(struct canfd_frame *cf, int maxdlen, speed_status_t *speed_status)
 {
     int len = (cf->len > maxdlen) ? maxdlen : cf->len;
+    double speed;
+    double delta;
+
     if (len < SPEED_POS + 1)
     {
         return;
     }
-    
-    speed_status->speed = (((cf->data[SPEED_POS] << 8) + cf->data[SPEED_POS + 1]) / 100) * 0.6213751;
+
+    speed = (((cf->data[SPEED_POS] << 8) + cf->data[SPEED_POS + 1]) / 100) * 0.6213751;
+    delta = speed - speed_status->speed;
+    if (-2 < delta && delta < 2)
+    {
+        speed_status->speed = speed;
+    }
 }
