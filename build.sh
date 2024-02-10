1be739dfb95b2e4c1a9566710c52915b0c33f29c
omer5144 <omer5144@gmail.com>
2024-02-10 07:57:12 -0800

    add gitignore

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+mkdir build
 gcc keyboard/*.c -o build/keyboard.out -I/usr/include/SDL2 -Wall -Wextra -lSDL2 -lSDL2_image -lSDL2_ttf
 gcc dashboard/*.c -o build/dashboard.out -I/usr/include/SDL2 -Wall -Wextra -lSDL2 -lSDL2_image -lSDL2_ttf
