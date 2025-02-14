331e36c1c97f3e4328756deab7ce02c03a7479a1
omer5 <omer5144@gmail.com>
2025-02-14 14:24:42 +0200

    add control volume

---
+++ solutions/06 - Radio/01 - control_volume.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import argparse
+import threading
+import enum
+import can
+
+
+VOLUME_ID = 0x28A
+
+
+def input_volume(interface: str) -> None:
+    try:
+        with can.Bus(
+            channel=interface,
+            bustype="socketcan",
+            can_filters=[{"can_id": VOLUME_ID, "can_mask": 0xFFF, "extended": False}],
+        ) as bus:
+            while True:
+                try:
+                    volume = int(input("Enter volume difference:   "))
+
+                    bus.send(
+                        can.Message(
+                            arbitration_id=VOLUME_ID,
+                            is_extended_id=False,
+                            data=volume.to_bytes(4, "big"),
+                        )
+                    )
+
+                except Exception:
+                    print("Invalid input")
+    finally:
+        pass
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("interface")
+    args = parser.parse_args()
+
+    try:
+        input_volume(args.interface)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
